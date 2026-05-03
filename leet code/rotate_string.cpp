8f80a48337a3dcd4cbdde28ea298366b7af78a09
manjotGG <manjotcharanjeet@gmail.com>
2026-05-04 00:00:51 +0530

    Add solution for Rotate String problem in C++

---
+++ leet code/rotate_string.cpp
@@ -0,0 +1,13 @@
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if (s.length() != goal.length()) return false;
+
+        string temp = s + s;
+
+        return temp.find(goal) != string::npos;
+    }
+};
