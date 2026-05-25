1dc7ddfb32ffc21f73484859496f0324b5b754f0
manjotGG <manjotcharanjeet@gmail.com>
2026-05-25 23:11:27 +0530

    Add solution for Jump Game VII problem in C++

---
+++ leet code/Jump_gameVII.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+class Solution {
+public:
+    bool canReach(string s, int minJump, int maxJump) {
+        int n = s.size();
+
+        vector<bool> dp(n, false);
+        dp[0] = true;
+
+        int reachable = 0;
+
+        for (int i = 1; i < n; i++) {
+
+            if (i - minJump >= 0 && dp[i - minJump]) {
+                reachable++;
+            }
+
+            if (i - maxJump - 1 >= 0 && dp[i - maxJump - 1]) {
+                reachable--;
+            }
+
+            if (s[i] == '0' && reachable > 0) {
+                dp[i] = true;
+            }
+        }
+
+        return dp[n - 1];
+    }
+};
