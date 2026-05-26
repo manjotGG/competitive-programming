482071da2be3fb54fa4de8c85e98ca0117c7e003
manjotGG <manjotcharanjeet@gmail.com>
2026-05-26 23:52:40 +0530

    Add solution for Count the Number of Special Characters problem in C++

---
+++ leet code/Count the_Number_of_Special_Characters_I.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <unordered_set>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    int numberOfSpecialChars(string word) {
+        unordered_set<char> lower, upper;
+
+        for (char ch : word) {
+            if (islower(ch))
+                lower.insert(ch);
+            else
+                upper.insert(ch);
+        }
+
+        int count = 0;
+
+        for (char ch = 'a'; ch <= 'z'; ch++) {
+            if (lower.count(ch) && upper.count(ch - 'a' + 'A')) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+};
+
+int main() {
+    Solution sol;
+
+    string word;
+    cin >> word;
+
+    cout << sol.numberOfSpecialChars(word) << endl;
+
+    return 0;
+}
