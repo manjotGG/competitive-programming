e2aa63b23033172e63e23298424776963d9a3410
manjotGG <manjotcharanjeet@gmail.com>
2026-06-20 23:26:44 +0530

    Add solution for Maximum Building Height problem in C++

---
+++ leet code/maximum-building-height.cpp
@@ -0,0 +1,58 @@
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int maxBuilding(int n, vector<vector<int>>& restrictions) {
+
+        restrictions.push_back({1, 0});
+        sort(restrictions.begin(), restrictions.end());
+
+        int m = restrictions.size();
+
+        // Left -> Right
+        for (int i = 1; i < m; i++) {
+            restrictions[i][1] = min(
+                restrictions[i][1],
+                restrictions[i - 1][1] +
+                restrictions[i][0] - restrictions[i - 1][0]
+            );
+        }
+
+        // Right -> Left
+        for (int i = m - 2; i >= 0; i--) {
+            restrictions[i][1] = min(
+                restrictions[i][1],
+                restrictions[i + 1][1] +
+                restrictions[i + 1][0] - restrictions[i][0]
+            );
+        }
+
+        long long ans = 0;
+
+        for (int i = 1; i < m; i++) {
+            long long x1 = restrictions[i - 1][0];
+            long long h1 = restrictions[i - 1][1];
+
+            long long x2 = restrictions[i][0];
+            long long h2 = restrictions[i][1];
+
+            long long dist = x2 - x1;
+
+            ans = max(
+                ans,
+                (h1 + h2 + dist) / 2
+            );
+        }
+
+        // Buildings after last restriction
+        long long lastPos = restrictions.back()[0];
+        long long lastHeight = restrictions.back()[1];
+
+        ans = max(
+            ans,
+            lastHeight + (long long)(n - lastPos)
+        );
+
+        return (int)ans;
+    }
+};
